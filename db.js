0b97f4322c2d6a44d9ebfe85d18d88b477887e5b
shabbeer <mdshabbeer707@gmail.com>
2024-06-20 19:10:07 +0530

    backend

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
-const mongoURI = "mongodb+srv://ishusaxena569:ADZacS26UScjSynK@cluster0.hbgomsf.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0"
+const mongoURI = "mongodb+srv://ishusaxena569:ADZacS26UScjSynK@cluster0.hbgomsf.mongodb.net/cluster0?retryWrites=true&w=majority&appName=Cluster0"
 
 
 const connectToMongo = () => {
