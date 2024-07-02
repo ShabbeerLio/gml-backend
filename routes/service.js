71c7333dfbc411b6fb7f8ce97e884b2e54a5c9a6
shabbeer <mdshabbeer707@gmail.com>
2024-07-02 17:47:31 +0530

    vercel changes next

--- routes/service.js
+++ routes/service.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
         cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`);
+        cb(null, `${Date.now() + '-' + file.originalname}`);
     }
 });
 
