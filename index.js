bf127fd4ebaf472eff261d87242e4d4b1a326fce
shabbeer <mdshabbeer707@gmail.com>
2024-07-03 11:23:22 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const connectToMongo = require('./db');
 const path = require('path');
-const fs = require('fs');
 
 // Connect to MongoDB
 connectToMongo();
@@ -10,26 +9,13 @@ connectToMongo();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) =>
-    res.json({message:"hello frorm Gmls api"})
-  );
-
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Static files route
-const uploadDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-}
-app.use('/uploads', express.static(uploadDir));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Log each request to /uploads
-app.use('/uploads', (req, res, next) => {
-    // console.log(`Request for file: ${req.url}`);
-    next();
-});
 // Available routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/service', require('./routes/service'));
